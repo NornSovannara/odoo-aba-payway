0695d244aa04391d7e86fed5ccab1356b653abb8
HUOT.Rithy <huot.rithy@ababank.com>
2026-02-27 10:01:19 +0700

    feat: verify webhook sign on pos module

--- aba_payway_qr_payment_pos_odoo/controllers/main.py
+++ aba_payway_qr_payment_pos_odoo/controllers/main.py
@@ -1,9 +1,11 @@
+import hmac
 import logging
 import pprint
 
+from werkzeug.exceptions import Forbidden
 from odoo import http
 from odoo.http import request
-
+from odoo.addons.aba_payway_base_config import const
 
 _logger = logging.getLogger(__name__)
 
@@ -19,11 +21,21 @@ class PayWayController(http.Controller):
             channel_name = 'pos.order.payment.payway.' + data['tran_id']
             
             # TODO: Verify webhook signature
-            # request.env['res.partner.bank'].sudo().with_context(active_test=False).search([
-            #     'xyz'='abc'
-            # ]).payway_key
-            # COMMENT - TODO: what field do I search to get the bank that user config payway in?
-            # I don't see any identifier that can use to search
+            
+            # PROBLEM: pos.payment does not exist before order completed
+            # So, I cannot retreive by transaction_id
+            # bank = request.env['pos.payment'].sudo().search([
+            #     ('transaction_id', '=', data['tran_id']),
+            # ], limit=1).payment_method_id.journal_id.bank_account_id
+
+            # As a workaround, I will search for any payment method with qr code method and get bank account from it
+            payment_method = request.env['pos.payment.method'].sudo().search([
+                ('qr_code_method', 'in', list(const.PAYMENT_METHODS_CODES))
+            ], limit=1)
+            bank_account = payment_method.journal_id.bank_account_id
+
+            received_signature = request.httprequest.headers.get('x-payway-hmac-sha512')
+            self._verify_notification_signature(data, received_signature, bank_account)
 
             # Send notification from backend
             request.env['bus.bus'].sudo()._sendone(
@@ -39,3 +51,27 @@ class PayWayController(http.Controller):
             _logger.exception("Unable to handle the webhook data.")
 
         return "OK"
+
+
+    @staticmethod
+    def _verify_notification_signature(notification_data, received_signature, bank_account):
+        """ Check that the received signature matches the expected one.
+
+        :param dict notification_data: The notification data
+        :param recordset bank_account: `res.partner.bank` record
+        :return: None
+        :raise: :class:`werkzeug.exceptions.Forbidden` if the signatures don't match
+        """
+
+        if not received_signature:
+            _logger.warning("Received notification with missing signature.")
+            raise Forbidden()
+        
+        _, _, api_key, _ = bank_account._payway_get_api_cred()
+        expected_signature = bank_account._payway_calculate_webhook_secure_hash(api_key, notification_data)
+        if (
+            expected_signature is None
+            or not hmac.compare_digest(received_signature, expected_signature)
+        ):
+            _logger.warning("Received notification with invalid signature.")
+            raise Forbidden()
